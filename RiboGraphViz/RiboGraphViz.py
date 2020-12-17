55745a9cff9541c9b6c64846ab4776e3b3facfc6
Hannah Wayment-Steele <hwayment@zwetschgen2.local>
2020-12-17 09:04:47 -0800

    update n_hairpins bug

--- RiboGraphViz/RiboGraphViz.py
+++ RiboGraphViz/RiboGraphViz.py
@@ -12,7 +12,7 @@ import numpy as np
 
 class RiboGraphViz(object):
 
-    def __init__(self, secstruct):
+    def __init__(self, secstruct, sequence=None):
         '''Create a NetworkX graph representing an RNA secondary structure, where nodes represent
         hairpin loops, internal loops, bulges, anything not a helix, and stems represent helices.
 
@@ -40,8 +40,9 @@ class RiboGraphViz(object):
             self.is_multi = True
         else:
             self.is_multi = False
+
         stems = utils.parse_stems_from_bps(utils.convert_structure_to_bps(self.secstruct))
-        self.stems = stems #[x for x in sorted(stems)]
+        self.stems = [x for x in sorted(stems)]
         self.stem_assignment = utils.get_stem_assignment(self.secstruct)
         self.pairmap = utils.get_pairmap(self.secstruct)
         self.G = nx.DiGraph()
@@ -49,16 +50,50 @@ class RiboGraphViz(object):
         self.helix_multiplier = 1
         self.helix_width = 1
         self.N = len(self.secstruct)
+        self.sequence=sequence
+
+        self.fiveprime_x, self.fiveprime_y, self.threeprime_x, self.threeprime_y = None, None, None, None
 
         self.setup_graph()
+        self.calc_MLD()
 
+        self.n_hairpins, self.n_internal_loops, self.n_3WJs, self.n_4WJs, self.n_5WJs_up = self.count_loops()
+
+    def get_53_distance(self):
+
+        plot_nodes = [n for n in list(self.G.nodes) if isinstance(n,str)]
+        subgraph = self.G.subgraph(plot_nodes)
+        subgraph = subgraph.to_undirected()
+
+        if not self.is_multi:
+            #locate 5' nucleotide
+            if 'n0' in list(subgraph.nodes()):
+                five_loc = 'n0'
+            else:
+                five_loc = 'h1b'
+
+            if 'n%d' % (self.N-1) in list(subgraph.nodes()):
+                three_loc = 'n%d' % (self.N-1)
+
+    def calc_MLD(self):
         nodes = [n for n in list(self.G.nodes)] # if not isinstance(n,str)]
         subgraph = self.G.subgraph(nodes).to_undirected()
-        node1 = [x for x in nx.traversal.bfs_edges(subgraph, 'h1a')][-1][-1]
-        node2 = [x for x in nx.traversal.bfs_edges(subgraph, node1)][-1][-1]
 
-        self.MLD = nx.shortest_path_length(subgraph, node1, node2, weight='MLD_weight')
-        self.n_hairpins, self.n_internal_loops, self.n_3WJs, self.n_4WJs, self.n_5WJs_up = self.count_loops()
+        first_helix_node = None
+        for n in nodes:
+            if isinstance(n, str):
+                if n.startswith('h'):
+                    first_helix_node = n
+                    break
+
+
+        if first_helix_node is None:
+            self.MLD = 0
+        else:
+            node1 = [x for x in nx.traversal.bfs_edges(subgraph, first_helix_node)][-1][-1]
+            node2 = [x for x in nx.traversal.bfs_edges(subgraph, node1)][-1][-1]
+
+            self.MLD = nx.shortest_path_length(subgraph, node1, node2, weight='MLD_weight')
 
     def setup_graph(self):
         '''Create graph by reading pairmap array and recursively creating edges.'''
@@ -82,6 +117,11 @@ class RiboGraphViz(object):
 
         stem_assignment_left = np.concatenate([np.array([-1]), self.stem_assignment[:-1]])
         stem_assignment_right = np.concatenate([self.stem_assignment[1:], np.array([-1])])
+
+        for nod in range(len(self.stem_assignment)):
+            if (nod,nod) in self.G.edges:
+
+                self.G.remove_edge(nod,nod)
 
         for i, pair in enumerate(self.pairmap):
             if pair==-1:
@@ -138,16 +178,20 @@ class RiboGraphViz(object):
                     stem_ind_2 = self.stem_assignment[i+1]
                     #color_ind = np.where(self.stem_assignment==stem_ind_1)[0][0]
 
-                    # print(stem_ind_1, stem_ind_2)
-                    # print(self.secstruct[i:i+2])
+                    #print("DEBUG", stem_ind_1, stem_ind_2)
+                    #print("DEBUG", self.secstruct[i:i+2])
 
                     if self.secstruct[i:i+2] == '((':
                         self.G.add_edge('h%da' % stem_ind_1,'h%db' % stem_ind_2, len=1, weight=1, MLD_weight=0)
+                        #self.G.add_edge(int(stem_ind_1), int(stem_ind_2)) # hkws hack dec 14
                     elif self.secstruct[i:i+2] == ')(':
                         if i+1 not in self.chainbreak:
                             self.G.add_edge('h%db' % stem_ind_1,'h%db' % stem_ind_2, len=1, weight=1, MLD_weight=0)
+                            #self.G.add_edge(stem_ind_1, stem_ind_2)
+
                     elif self.secstruct[i:i+2] == '))':
                         self.G.add_edge('h%db' % stem_ind_1,'h%da' % stem_ind_2, len=1, weight=1, MLD_weight=0)
+                        #self.G.add_edge(stem_ind_1, stem_ind_2)
 
     def add_edges_r_(self, start_index, end_index, last_helix, last_loop):
         '''Recursive method to add edges to graph.'''
@@ -156,18 +200,25 @@ class RiboGraphViz(object):
             print('Error, start_index > end_index')
             sys.exit(0)
             
-        if(self.pairmap[start_index] == end_index):
-            #print("we're helix %d! keep going" % stem_assignment[start_index])
+        if (self.pairmap[start_index] == end_index):
+            #print("we're in helix %d! keep going" % self.stem_assignment[start_index])
+
             self.add_edges_r_(start_index+1, end_index-1, self.stem_assignment[start_index], last_loop)
+
         else:
             jj = start_index
             while jj <=end_index:
                 if self.pairmap[jj] > jj:
+                    #print('where are we?')
+                    self.G.add_edge(int(last_loop), int(last_helix)) #int(self.stem_assignment[jj])) # needs more attributes?
+                    last_loop = copy(last_helix)
+
                     self.add_edges_r_(jj, self.pairmap[jj],self.stem_assignment[jj], last_loop)
                     jj = self.pairmap[jj] + 1 #leaving helix
                 else:
-                    #print(jj,'in a loop!')
-                    #print('Last helix was %d, last loop was %d' % (last_helix, last_loop))
+                    #print("DEBUG", jj,'in a loop!')
+                    #print('DEBUG, Last helix was %d, last loop was %d' % (last_helix, last_loop))
+                    #print('DEBUG', self.G.nodes)
                     if last_helix not in list(self.G.nodes):
                         #print(last_helix, len(stems[int(last_helix-1)]))
                         self.G.add_edge(int(last_loop), int(last_helix),
@@ -184,35 +235,16 @@ class RiboGraphViz(object):
 
                     jj+=1
 
-    def draw(self, node_scaling = 1, label=None, struct_label=None, line=False,
-         c = None, cmap='plasma', align=False, alpha=None, ax=None, fontsize=12,
-         align_mode='COM'):
+    def get_coordinates(self, align=False, align_mode='COM'):
         '''
-        Draw structure using GraphViz layout.
-        node_scaling: change scale of nucleotide circles.
-        label (str): list of labels for nucleotides.
-        c: color. Can be single matplotlib color letter, string of matplotlib color letters,
-                        or list-like object of values (to be used with cmap).
-        alpha: Transparency value. can be single value or vector.
         align (bool): set first nucleotide at [0,0] and rotates structure according to align_mode.
         align_mode ("COM","end"): if 'COM', aligns center of mass to x axis. if "end", aligns 3' end to x axis.
-        ax: axis object to use for drawing.
-        figsize: figure size.
-        fontsize (int): fontsize for labels.
-
-        Returns: ax object.
+        Return: array of x_coords, array of y_coords 
         '''
-        
+
         if self.is_multi and align:
             raise RuntimeError('Alignment and multiple structures not yet implemented.')
 
-        if struct_label is not None:
-            if self.is_multi:
-                assert len(struct_label) == len(self.chainbreak)+1
-            else:
-                raise RuntimeError('struct_label present and not multiple structures.')
-
-        N = len(self.secstruct)
         plot_nodes = [n for n in list(self.G.nodes) if isinstance(n,str)]
         subgraph = self.G.subgraph(plot_nodes)
         subgraph = subgraph.to_undirected()
@@ -226,16 +258,16 @@ class RiboGraphViz(object):
 
             #locate 3' nucleotide. Only located if in a distinct string, otherwise can't distinguish end of outer stem.
 
-            if 'n%d' % (N-1) in list(subgraph.nodes()):
-                subgraph.add_edge('n%d' % (N-1),"3'",len=2)
+            if 'n%d' % (self.N-1) in list(subgraph.nodes()):
+                subgraph.add_edge('n%d' % (self.N-1),"3'",len=2)
 
         pos = graphviz_layout(subgraph, prog='neato')
 
         if not self.is_multi:
-            fiveprime_x, fiveprime_y = pos["5'"]
+            self.fiveprime_x, self.fiveprime_y = pos["5'"]
 
             if "3'" in list(subgraph.nodes()):
-                threeprime_x, threeprime_y = pos["3'"]
+                self.threeprime_x, self.threeprime_y = pos["3'"]
 
 
         for (u,v) in list(subgraph.edges()):
@@ -284,14 +316,21 @@ class RiboGraphViz(object):
                     node_positions_y[stem[j][0]] = y_midpoint+y_diff
                     node_positions_y[stem[j][1]] = y_midpoint-y_diff
 
+        if self.threeprime_x is not None:
+            self.threeprime_x = self.threeprime_x - node_positions_x[0]
+            self.threeprime_y = self.threeprime_y - node_positions_y[0]
+
+        if self.fiveprime_x is not None:
+            self.fiveprime_x = self.fiveprime_x - node_positions_x[0]
+            self.fiveprime_y = self.fiveprime_y - node_positions_y[0]
+
+        node_pos_list_x = [node_positions_x[i]-node_positions_x[0] for i in range(self.N)]
+        node_pos_list_y = [node_positions_y[i]-node_positions_y[0] for i in range(self.N)]
+
         if align:
-
-            node_pos_list_x = [node_positions_x[i]-node_positions_x[0] for i in range(N)]
-            node_pos_list_y = [node_positions_y[i]-node_positions_y[0] for i in range(N)]
-
             if align_mode=='end':
-                vec_01_x = node_pos_list_x[N-1]
-                vec_01_y = node_pos_list_y[N-1]
+                vec_01_x = node_pos_list_x[self.N-1]
+                vec_01_y = node_pos_list_y[self.N-1]
             elif align_mode=='COM':
                 vec_01_x = np.mean(node_pos_list_x)
                 vec_01_y = np.mean(node_pos_list_y)
@@ -301,48 +340,107 @@ class RiboGraphViz(object):
 
             curr_angle = np.arctan2(vec_01_y, vec_01_x)
 
-            new_node_pos_list_x = [np.cos(-1*curr_angle)*node_pos_list_x[i] - np.sin(-1*curr_angle) *node_pos_list_y[i] for i in range(N)]
-            new_node_pos_list_y = [np.sin(-1*curr_angle)*node_pos_list_x[i] + np.cos(-1*curr_angle) *node_pos_list_y[i] for i in range(N)]
-            
+            new_node_pos_list_x = [np.cos(-1*curr_angle)*node_pos_list_x[i] - np.sin(-1*curr_angle) *node_pos_list_y[i] for i in range(self.N)]
+            new_node_pos_list_y = [np.sin(-1*curr_angle)*node_pos_list_x[i] + np.cos(-1*curr_angle) *node_pos_list_y[i] for i in range(self.N)]
+
+            if self.fiveprime_x is not None:
+                oldx, oldy = copy(self.fiveprime_x), copy(self.fiveprime_y)
+                self.fiveprime_x = np.cos(-1*curr_angle)*oldx - np.sin(-1*curr_angle) *oldy
+                self.fiveprime_y = np.sin(-1*curr_angle)*oldx + np.cos(-1*curr_angle) *oldy
+
+            if self.threeprime_x is not None:
+                oldx, oldy = copy(self.threeprime_x), copy(self.threeprime_y)
+                self.threeprime_x = np.cos(-1*curr_angle)*oldx - np.sin(-1*curr_angle) *oldy
+                self.threeprime_y = np.sin(-1*curr_angle)*oldx + np.cos(-1*curr_angle) *oldy
+
             if np.mean(new_node_pos_list_y) < 0:
                 new_node_pos_list_y = [-1*y for y in new_node_pos_list_y]
+                if self.threeprime_y is not None: self.threeprime_y *= -1
+                if self.fiveprime_y is not None: self.fiveprime_y *= -1
+
             if new_node_pos_list_y[1] < new_node_pos_list_y[0]:
                 new_node_pos_list_y = [-1*y for y in new_node_pos_list_y]
+                if self.threeprime_y is not None: self.threeprime_y *= -1
+                if self.fiveprime_y is not None: self.fiveprime_y *= -1
+
+            node_pos_list_y = new_node_pos_list_y
+            node_pos_list_x = new_node_pos_list_x
                 
-        else:
-            if not self.is_multi:
-                if "3'" in list(subgraph.nodes()):
-                    threeprime_x = threeprime_x - node_positions_x[0]
-                    threeprime_y = threeprime_y - node_positions_y[0]
 
-                fiveprime_x = fiveprime_x - node_positions_x[0]
-                fiveprime_y = fiveprime_y - node_positions_y[0]
+        if self.threeprime_x is not None:
+            self.threeprime_x /= bond_width
+            self.threeprime_y /= bond_width
 
-            new_node_pos_list_x = [node_positions_x[i]-node_positions_x[0] for i in range(N)]
-            new_node_pos_list_y = [node_positions_y[i]-node_positions_y[0] for i in range(N)]
+        if self.fiveprime_x is not None:
+            self.fiveprime_x /= bond_width
+            self.fiveprime_y /= bond_width
 
-        fig = plt.gcf()
+        # Normalize to bond width
+        node_pos_list_x /= bond_width
+        node_pos_list_y /= bond_width
+
+
+        return node_pos_list_x, node_pos_list_y
+
+
+
+    def draw(self, label=None, struct_label=None, line=False,
+        align=False, align_mode='COM', x0=0, y0=0,
+         c = None, cmap='plasma', alpha=None, ax=None,
+         vmin=None, vmax=None, fontsize=12):
+
+        '''
+        Draw structure using GraphViz layout.
+
+        Input: RGV object or list of objects.
+
+        label (str): list of labels for nucleotides.
+        c: color. Can be single matplotlib color letter, string of matplotlib color letters,
+                        or list-like object of values (to be used with cmap).
+        alpha: Transparency value. can be single value or vector.
+        
+        ax: axis object to use for drawing.
+        figsize: figure size.
+        fontsize (int): fontsize for labels.
+
+        Returns: ax object.
+        '''
+        if struct_label is not None:
+            if self.is_multi:
+                assert len(struct_label) == len(self.chainbreak)+1
+            else:
+                raise RuntimeError('struct_label present and not multiple structures.')
+
+
+        x_coords, y_coords = self.get_coordinates(align=align, align_mode=align_mode)
+
+        #fig = plt.gcf()
         ax = ax or plt.gca()
         ax.set_aspect('equal')
 
-
         if isinstance(c,str):
             if len(c) == 1:
-                colors=[c]*N
+                colors=[c]*self.N
             else:
-                assert len(c) == N
+                assert len(c) == self.N
                 colors=c
+
         elif c is None:
-            colors=['k']*N
+            colors=['k']*self.N
+
         else:
-            assert len(c) == N
+            assert len(c) == self.N
             colormap = plt.get_cmap(cmap) 
-            cNorm  = mcolors.Normalize(vmin=np.min(c), vmax=np.max(c)) #3 for reac
+            if vmin is None:
+                vmin = np.min(c)
+            if vmax is None:
+                vmax = np.max(c)
+            cNorm  = mcolors.Normalize(vmin=vmin, vmax=vmax) #3 for reac
             scalarMap = cm.ScalarMappable(norm=cNorm, cmap=colormap)
             colors = [scalarMap.to_rgba(val) for val in c]
 
-        ax.set_xlim([np.min(new_node_pos_list_x)-100, np.max(new_node_pos_list_x)+100])
-        ax.set_ylim([np.min(new_node_pos_list_y)-100, np.max(new_node_pos_list_y)+100])
+        ax.set_xlim([np.min(x_coords)-5+x0, np.max(x_coords)+5+x0])
+        ax.set_ylim([np.min(y_coords)-5+y0, np.max(y_coords)+5+y0])
 
         # Line mode: (coming)
         # for u,v in G.edges():
@@ -353,32 +451,30 @@ class RiboGraphViz(object):
         #     ax.add_line(l)
 
         if alpha is None:
-            alpha=[1]*N
+            alpha=[1]*self.N
         elif isinstance(alpha,float):
-            alpha = [alpha]*N
+            alpha = [alpha]*self.N
         elif isinstance(alpha, list):
-            assert len(alpha)==N
+            assert len(alpha)==self.N
 
-        for i in range(N):
-            circ = plt.Circle((new_node_pos_list_x[i], new_node_pos_list_y[i]), radius=bond_width/2, color=colors[i], alpha=alpha[i],linewidth=0)
+        for i in range(self.N):
+            circ = plt.Circle((x_coords[i]+x0, y_coords[i]+y0), radius=1/2, color=colors[i], alpha=alpha[i],linewidth=0)
             ax.add_artist(circ)
             if label:
-                plt.text(new_node_pos_list_x[i], new_node_pos_list_y[i], label[i],
+                plt.text(x_coords[i]+x0, y_coords[i]+y0, label[i],
                  horizontalalignment='center', verticalalignment='center', fontsize=fontsize)
         #plt.scatter(new_node_pos_list_x, new_node_pos_list_y, s=1, c = c, cmap=cmap)
 
-        if not align and not self.is_multi:
-            plt.text(fiveprime_x, fiveprime_y, "5'")
+        if self.fiveprime_x is not None:
+            plt.text(self.fiveprime_x+x0, self.fiveprime_y+y0, "5'")
 
-            if "3'" in list(subgraph.nodes()):
-                plt.text(threeprime_x, threeprime_y, "3'")
-
+        if self.threeprime_x is not None:
+            plt.text(self.threeprime_x+x0, self.threeprime_y+y0, "3'")
 
         if struct_label and self.is_multi:
             label_pos = [0]+self.chainbreak
-            for i, x in enumerate(struct_label):
-                plt.text(new_node_pos_list_x[label_pos[i]],new_node_pos_list_y[label_pos[i]], x)
-
+            for i, text in enumerate(struct_label):
+                plt.text(x_coords[label_pos[i]]+x0, y_coords[label_pos[i]]+y0, text)
 
         ax.axis('off')
 
@@ -403,7 +499,7 @@ class RiboGraphViz(object):
                 n_4 += 1
             elif x[1] > 4:
                 n_5 += 1
-        return n_1, n_2, n_3, n_4, n_5
+        return n_1 - 1, n_2, n_3, n_4, n_5 #subtract off 1 to not count exterior loop as hairpin
 
     def get_info(self):
         print("Max ladder distance: %d" % self.MLD)
@@ -412,5 +508,4 @@ class RiboGraphViz(object):
         print("n_3WJs: %d" % self.n_3WJs)
         print("n_4WJs: %d" % self.n_4WJs)
         print("n_5WJs_up: %d" % self.n_5WJs_up)
-
 
